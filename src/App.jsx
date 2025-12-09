597720202738740758ee3e8de2c5f729801aa9e9
tefrancisco <dzfranciscodz@gmail.com>
2025-12-09 20:58:57 -0300

    Melhorando a interface visual, aumentando as fontes para melhor visibilidade

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
+import { calculateInvestmentResults, formatter } from './util/investment.js'
 import Header from './components/Header.jsx'
 import TableRow from './components/TableRow.jsx'
 import Input from './components/Input.jsx'
-import { calculateInvestmentResults, formatter } from './util/investment.js'
 
 function App() {
+  // State que vai armazenar todos os parâmetros do investimento em um só objeto.
   const [investmentData, setInvestmentData] = useState({
     initialInvestment: '',
     annualInvestment: '',
@@ -12,8 +13,10 @@ function App() {
     duration: '',
   })
 
+  // Array de previsões do investimento retornado pela função.
   let annualData = calculateInvestmentResults(investmentData)
 
+  // Função que trata os dados dos inputs via two-way-binding e atualiza o objeto do state que armazena os dados do investimento.
   function handleUserInput(event, par) {
     setInvestmentData((prevData) => {
       const updatedData = { ...prevData }
@@ -51,9 +54,6 @@ function App() {
 
           <Input label="Annual investment" value={investmentData.annualInvestment} onChangeFunction={() => handleUserInput(event, 'anualInv')} />
 
-        </div>
-        <div className="input-group">
-
           <Input label="Expected return" value={investmentData.expectedReturn} onChangeFunction={() => handleUserInput(event, 'expectedRet')} />
 
           <Input label="Duration" value={investmentData.duration} onChangeFunction={() => handleUserInput(event, 'duration')} />
@@ -72,6 +72,8 @@ function App() {
           </tr>
         </thead>
         <tbody>
+          {/* Map do array que contém os objetos que representam os resultados do investimento de cada ano, criando um component para cada e passando as props
+          ao desconstruir o objeto. */}
           {annualData.map((obj) => <TableRow key={obj.year} {...obj} />)}
         </tbody>
       </table>
