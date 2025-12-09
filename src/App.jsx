770c17c44bdee9562884823213ed7d051118bac6
tefrancisco <dzfranciscodz@gmail.com>
2025-12-09 10:38:32 -0300

    Creating TableRow component, adjusting investment.js function, displaying investment results

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,82 @@
+import { useState } from 'react'
+import Header from './components/Header.jsx'
+import TableRow from './components/TableRow.jsx'
+import { calculateInvestmentResults, formatter } from './util/investment.js'
+
 function App() {
+  const [investmentData, setInvestmentData] = useState({
+    initialInvestment: '',
+    annualInvestment: '',
+    expectedReturn: '',
+    duration: '',
+  })
+
+  let annualData = calculateInvestmentResults(investmentData)
+  console.log(annualData)
+
+  console.log(investmentData)
+
+  function handleUserInput(event, par) {
+    setInvestmentData((prevData) => {
+      const updatedData = { ...prevData }
+
+      if(par === 'initialInv') {
+        updatedData.initialInvestment = Number(event.target.value)
+      }
+
+      else if(par === 'anualInv') {
+        updatedData.annualInvestment = Number(event.target.value)
+      }
+
+      else if(par === 'expectedRet') {
+        updatedData.expectedReturn = Number(event.target.value)
+      }
+
+      else if(par === 'duration') {
+        updatedData.duration = Number(event.target.value)
+      }
+
+      return updatedData;
+    })
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+      <Header>
+        <img src="/investment-calculator-logo.png" alt="" />
+        <h1>React Investment Calculator</h1>
+      </Header>
+
+      <section id="user-input">
+        <div className="input-group">
+          <label htmlFor="">Initial investment</label>
+          <input type="number" value={investmentData.initialInvestment} onChange={() => handleUserInput(event, 'initialInv')} />
+          <label htmlFor="">Annual investment</label>
+          <input type="number" value={investmentData.annualInvestment} onChange={() => handleUserInput(event, 'anualInv')} />
+        </div>
+        <div className="input-group">
+          <label htmlFor="">Expected return</label>
+          <input type="number" value={investmentData.expectedReturn} onChange={() => handleUserInput(event, 'expectedRet')} />
+          <label htmlFor="">Duration</label>
+          <input type="number" value={investmentData.duration} onChange={() => handleUserInput(event, 'duration')} />
+        </div>
+      </section>
+
+      <table id="result">
+        <thead>
+          <tr>
+            <td>Year</td>
+            <td>Investment Value</td>
+            <td>Interest (Year)</td>
+            <td>Total Interest</td>
+            <td>Invested Capital</td>
+          </tr>
+        </thead>
+        <tbody>
+          {annualData.map((obj) => <TableRow key={obj.year} {...obj} />)}
+        </tbody>
+      </table>
+    </>
   )
 }
 
--- src/util/investment.js
+++ src/util/investment.js
@@ -12,15 +12,19 @@ export function calculateInvestmentResults({
 }) {
   const annualData = [];
   let investmentValue = initialInvestment;
+  let totalInterest = 0
 
   for (let i = 0; i < duration; i++) {
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
+    totalInterest = totalInterest + interestEarnedInYear 
     annualData.push({
       year: i + 1, // year identifier
       interest: interestEarnedInYear, // the amount of interest earned in this year
       valueEndOfYear: investmentValue, // investment value at end of year
       annualInvestment: annualInvestment, // investment added in this year
+      totalInterest: totalInterest,
+      investedCapital: initialInvestment + (annualInvestment * (i + 1))
     });
   }
 
